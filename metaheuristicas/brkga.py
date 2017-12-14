c84d1887e7778acb27afa9fff65b94c1603436b3
pique <jordi.pique.selles@est.fib.upc.edu>
2017-12-14 11:50:32 +0100

    All BRKGA finished, but needs testing

--- metaheuristicas/brkga.py
+++ metaheuristicas/brkga.py
@@ -5,71 +5,65 @@ from otherScripts.checkingFunctions import *
 
 
 class Brkga:
-    def __init__(self, numIndividuals, chrLength, decoder):
-        self.numIndividuals = numIndividuals
-        self.chrLength = chrLength
-        self.decoder = decoder
-        self.population = self._initializePopulation()
+    def __init__(self, decode):
+        self.decode = decode
+        self.population = []
 
-
-    def run(self, maxGenerations, data):
+    def run(self, data, chrLength, numIndividuals=100, maxGenerations=1000, eliteProp=0.1, mutantsProp=0.2,
+            inheritanceProp=0.7):
+        numElite = int(math.ceil(numIndividuals * eliteProp))
+        numMutants = int(math.ceil(numIndividuals * mutantsProp))
+        numCrossover = max(numIndividuals - numElite - numMutants, 0)
+        self.population = self._initializePopulation(numIndividuals, chrLength)
         evol = []
-        for _ in range(maxGenerations):
-            self.population = self.decoder.decode(self.population, data)
-            evol.append(self.getBestFitness(self.population)['fitness'])
-            if numElite > 0:
-                elite, nonelite = self.classifyIndividuals(self.population, numElite)
-            else:
-                elite = []
-                nonelite = self.population
-            if numMutants > 0:
-                mutants = self.generateMutantIndividuals(numMutants, chrLength)
-            else:
-                mutants = []
-            if numCrossover > 0:
-                crossover = self.doCrossover(elite, nonelite, ro, numCrossover)
-            else:
-                crossover = []
-            population = elite + crossover + mutants
 
-        population = self.decoder.decode(population, data)
-        bestIndividual = self.getBestFitness(population)
+        for _ in range(maxGenerations):
+            self.population = self.decode(self.population, data)
+            evol.append(self._getBestFitness()['fitness'])
+
+            elite, nonelite = self._classifyIndividuals(numElite)
+            mutants = self._generateMutantIndividuals(numMutants, chrLength)
+            crossover = self._doCrossover(elite, nonelite, inheritanceProp, numCrossover)
+            self.population = elite + crossover + mutants
+
+        self.population = self.decode(self.population, data)
+        bestIndividual = self._getBestFitness()
         plt.plot(evol)
         plt.xlabel('number of generations')
         plt.ylabel('Fitness of best individual')
-        plt.axis([0, len(evol), 0, (self.chrLength + 1) * self.chrLength / 2])
+        plt.axis([0, len(evol), 0, (chrLength + 1) * chrLength / 2])
         plt.show()
 
         print(bestIndividual)
 
-    def _initializePopulation(self):
+    @staticmethod
+    def _initializePopulation(numIndividuals, chrLength):
         population = []
-        for i in range(self.numIndividuals):
-            chromosome = list(np.random.rand(self.chrLength))
+        for i in range(numIndividuals):
+            chromosome = list(np.random.rand(chrLength))
             population.append({'chr': chromosome, 'solution': {}, 'fitness': None})
         return population
 
-
-    def classifyIndividuals(population, numElite):
-        fitness = np.array([e['fitness'] for e in population])
+    def _classifyIndividuals(self, numElite):
+        fitness = np.array([e['fitness'] for e in self.population])
         order = sorted(range(len(fitness)), key=lambda k: fitness[k])
         whichElite = order[0:numElite]
         whichNonElite = order[numElite:(len(fitness))]
-        population = np.array(population)
+        population = np.array(self.population)
         elite = population[whichElite]
         nonElite = population[whichNonElite]
         return list(elite), list(nonElite)
 
-
-    def generateMutantIndividuals(numMutants, chrLength):
+    @staticmethod
+    def _generateMutantIndividuals(numMutants, chrLength):
         mutants = []
         for i in range(numMutants):
             chromosome = list(np.random.rand(chrLength))
             mutants.append({'chr': chromosome, 'solution': {}, 'fitness': None})
         return mutants
 
-
-    def doCrossover(elite, nonelite, ro, numCrossover):
+    @staticmethod
+    def _doCrossover(elite, nonelite, ro, numCrossover):
         crossover = []
         for i in range(numCrossover):
             indexElite = int(math.floor(np.random.rand() * len(elite)))
@@ -81,71 +75,98 @@ class Brkga:
             crossover.append({'chr': chrCross, 'solution': {}, 'fitness': None})
         return crossover
 
+    def _getBestFitness(self):
+        # fitness = np.array([e['fitness'] for e in population])
+        # order = sorted(range(len(fitness)), key=lambda k: fitness[k])
+        # return population[order[0]]
+        bestFit = min(self.population, key=lambda x: x['fitness'])
+        return bestFit
 
-    def getBestFitness(population):
-        fitness = np.array([e['fitness'] for e in population])
-        order = sorted(range(len(fitness)), key=lambda k: fitness[k])
-        return population[order[0]]
+
+def getChrLength(params):
+    # Max number of groups of consecutive working hours
+    maxNumGroupConsecH = params['maxPresence'] - params['maxHours'] + 1
+    # Length of an encoded nurse
+    legthEncodedNurse = maxNumGroupConsecH + 2
+    return params['numNurses'] * legthEncodedNurse
 
 
-def decoder(population, params):
+def decode(population, params, verbose=False):
     hoursDay = params['hoursDay']
-    minHours = params["minHours"]
+    # minHours = params["minHours"]
     maxHours = params["maxHours"]
     maxConsec = params["maxConsec"]
     maxPresence = params["maxPresence"]
+    demand = params["demand"]
+    numNurses = params['numNurses']
 
-    # qMax = int(math.ceil((maxPresence + 1) / (maxConsec + 1)))
-    qMax = maxPresence - maxHours + 1
+    # Max number of groups of consecutive working hours
+    maxNumGroupConsecH = maxPresence - maxHours + 1
+    # Length of an encoded nurse
+    legthEncodedNurse = maxNumGroupConsecH + 2
+    # Last hour when the working day of a nurse can start
     lastInitHour = hoursDay - maxPresence
-    # lastInitHour = hoursDay - minHours - math.ceil(minHours/maxHours) + 1
 
-    listNurses = []
-    for elem in population:
+    listSolutions = []
+    for encodedSetNurses in population:
         nurses = []
-        for i in range(0, len(elem), qMax+1):
-            initHour = int(round(elem[i] * lastInitHour, 0))
-            # q = min(qMax, int(math.ceil((hoursDay - initHour + 1) / (maxConsec + 1))))
-            q = qMax
-            encodedNurse = elem[i+1:i+q+1]
-            workedHours = maxHours
-            s = sum(encodedNurse)
-            aux = [x/s for x in encodedNurse]
-            chunksHours = [0] * len(aux)
-            for j in range(len(aux)):
-                chunksHours[j] = min(maxConsec, int(aux[j]*workedHours))
-                if chunksHours[j] == maxConsec:
-                    aux[j] = 0
-                else:
-                    aux[j] -= chunksHours[j] / workedHours
+        for i in range(0, len(encodedSetNurses), legthEncodedNurse):
+            encodedNurse = encodedSetNurses[i:legthEncodedNurse]
+            works = encodedNurse[0] >= 0.5  # if the nurse works
+            initHour = int(round(encodedNurse[1] * lastInitHour, 0))  # the hour she starts working
+            encodedConsecHours = encodedNurse[2:legthEncodedNurse]  # the chunks of consecutive hours encoded
+            workedHours = maxHours  # number of hours the nurse works
+            s = sum(encodedConsecHours)
+            normEncodedNurse = [x/s for x in encodedConsecHours]
+            chunksHours = [0] * len(normEncodedNurse)  # length of each group of consecutive hours
 
-            sortedAux = sorted(enumerate(aux), key=lambda x: x[1], reverse=True)
-            remainingHours = workedHours - sum(chunksHours)
-            while remainingHours > 0:
-                for (j, x) in sortedAux:
-                    if x != 0:
-                        chunksHours[j] += 1
-                        remainingHours -= 1
-                        if remainingHours == 0:
-                            break
+            if works:
+                # Assign proportionally to each element of normEncodedNurse the length of a group of consecutive hours
+                for j in range(len(normEncodedNurse)):
+                    chunksHours[j] = min(maxConsec, int(normEncodedNurse[j]*workedHours))
+                    if chunksHours[j] == maxConsec:
+                        normEncodedNurse[j] = 0
+                    else:
+                        normEncodedNurse[j] -= chunksHours[j] / workedHours
 
+                # Assign the remaining hours to the groups of consecutive hours that are not full
+                sortedAux = sorted(enumerate(normEncodedNurse), key=lambda x: x[1], reverse=True)
+                remainingHours = workedHours - sum(chunksHours)
+                # I have to be sure that this will not become an infinite loop
+                while remainingHours > 0:
+                    for (j, x) in sortedAux:
+                        if x != 0 and chunksHours[j] < maxConsec:
+                            chunksHours[j] += 1
+                            remainingHours -= 1
+                            if remainingHours == 0:
+                                break
+
+            # Construct the nurse
             nurse = [0] * initHour
             for chunk in chunksHours:
                 if chunk != 0:
                     nurse += [1] * chunk + [0]
             nurse += [0] * hoursDay
             nurse = nurse[:hoursDay]
-            print(nurse, sum(nurse))
+            if verbose:
+                print(nurse, sum(nurse))
             nurses.append(nurse)
 
-        print()
-        listNurses.append(nurses)
+        if verbose:
+            print()
 
-    return listNurses
+        # calculate the dictionary we'll add to listSolutions
+        nWorkNurses = sum(sum(n) > 0 for n in nurses)
+        offer = getOffer(nurses)
+        uncovDemand = sum(min(0, demand[i] - offer[i]) for i in range(len(demand)))
+        solution = {'chr': encodedSetNurses, 'solution': nurses, 'fitness': uncovDemand*numNurses + nWorkNurses}
+        listSolutions.append(solution)
+
+    return listSolutions
 
 
 def proves():
-    params = {}
+    params = dict()
     params['hoursDay'] = 24
     params["maxHours"] = 8
     params["maxConsec"] = 4
@@ -154,12 +175,14 @@ def proves():
     params["minHours"] = 4
     params['numNurses'] = 100
 
-    qMax = int(math.ceil((params["maxPresence"] + 1) / (params["maxConsec"] + 1)))
-    population = np.random.rand((qMax+1)*params['numNurses'])
+    maxNumGroupConsecH = params["maxPresence"] - params["maxHours"] + 1
+    population = np.random.rand((maxNumGroupConsecH+2)*params['numNurses'])
 
-    listNurses = decoder([population], params)
+    listNurses = decode([population], params)
     for nurses in listNurses:
         print(answerSatisfiesConstr(nurses, params))
         print(getOffer(nurses))
 
-proves()
+
+if __name__ == '__main__':
+    proves()
